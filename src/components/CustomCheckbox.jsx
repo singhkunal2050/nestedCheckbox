e5e82da118784707484d4e2689a93253294b76fb
singhkunal2050 <singhkunal2050@gmail.com>
2022-07-10 15:55:38 +0530

    toggle state bugs fixed 🐛

--- src/components/CustomCheckbox.jsx
+++ src/components/CustomCheckbox.jsx
@@ -4,35 +4,41 @@ import data from '../data/data.js'
 
 function CustomCheckbox({ parentId }) {
   const [checkboxState, setcheckboxState] = useState(data)
-  const [expanded , setexpanded] = useState(true);
+  const [expanded, setexpanded] = useState([]);
 
   const handleToggle = (e) => {
-    console.log(e)
-    setexpanded(!expanded)
+    let current = e.target.dataset.name
+    console.log(current)
+    if(expanded.includes(current)){
+      console.log('gotit')
+      setexpanded([...expanded.filter(elem=>elem!=current)])
+    }else{
+       setexpanded([...expanded , current])
+    }
   }
 
-  const hasChildren = (name) =>{
-    let results = checkboxState.filter(elem=>elem.parentId==name)
+  const hasChildren = (name) => {
+    let results = checkboxState.filter(elem => elem.parentId == name)
     return results.length > 0
   }
 
   const getChildren = (name) => {
-    let children = checkboxState.filter(elem=>elem.parentId==name)
-    return children.map(elem => <CustomCheckbox parentId={elem.parentId} /> )
+    let children = checkboxState.filter(elem => elem.parentId == name)
+    return children.map(elem => <CustomCheckbox parentId={elem.parentId} />)
   }
 
-  const idToRender = data.filter(elem=>elem.parentId==parentId);
+  const idToRender = data.filter(elem => elem.parentId == parentId);
   let obj = {};
   idToRender.forEach((e) => obj[e] = { checked: false, intermediate: false })
 
-  console.log({data , idToRender , m: Math.random() })
+  console.log({ data, idToRender, m: Math.random() })
   return (
     <>
       {idToRender.map(elem => {
-        return <div className="list" key={elem.name }>
-          <span className='icon' onClick={handleToggle} data-has-child={ hasChildren(elem.name)}>{expanded ? "-" : "+"}</span>
-          <input type="checkbox" checked={false} onChange={()=>{}} className={expanded ? 'expanded' : '' }   /> {elem.name}
-          {expanded && getChildren(elem.name)}
+        return <div className="list" key={elem.name}>
+          <span className='icon' data-name={elem.name} onClick={handleToggle} data-has-child={hasChildren(elem.name)}>{expanded.includes(elem.name)  ? "-" : "+"}</span>
+          <input type="checkbox" value={elem.name} checked={false} onChange={() => { }} className={expanded.includes(elem.name) ? 'expanded' : ''} /> {elem.name}
+          {expanded.includes(elem.name) && getChildren(elem.name)}
         </div>
         // return <Checkbox  />
       })}
