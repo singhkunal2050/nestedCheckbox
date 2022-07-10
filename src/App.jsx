facc0dc23c5f7851915672a75cecc771bced1104
singhkunal2050 <singhkunal2050@gmail.com>
2022-07-10 20:09:36 +0530

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,14 @@ function App() {
 
   return (
     <div className='app'>
-      <h4 class="heading">Custom Checkbox by  <a href="https://singhkunal2050.dev/" target="_blank"> singhkunal2050 </a> </h4>
+      <h4 class="heading">Custom Recursive Nested Checkbox by  <a href="https://singhkunal2050.dev/" target="_blank"> singhkunal2050 </a> </h4>
       <CustomCheckbox parentId={null} />
+
+      <footer>
+        <p> Fork me on Github <a target="_blank" href="https://github.com/singhkunal2050/nestedCheckbox">https://github.com/singhkunal2050/nestedCheckbox</a>
+        </p>
+      </footer>
+
     </div>
   )
 }
